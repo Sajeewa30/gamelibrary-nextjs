2a87c22ef1960c29a1c023c9aa35054009dd971a
Sajeewa30 <sajeewadilshan30@gmail.com>
2025-05-31 15:58:36 +0530

    just started developing the api after creating form

--- app/2025/page.tsx
+++ app/2025/page.tsx
@@ -14,16 +14,21 @@ const TwentyTwentyFive = () => {
     <div className="w-[90%] mx-auto text-center pt-[50px] bg-[linear-gradient(rgba(4,9,30,0.8),rgba(4,9,30,0.8)),url('/background.JPG')] bg-cover bg-center">
         <div className="mt-[1%] flex flex-row justify-center flex-wrap">
             <div className='mt-[1%] flex flex-row justify-center flex-wrap'>
-
-                <Game name='Hellblade' year='2024' imageUrl='/2025/2025_1.jpg' specialDescription='This is a Single Player Story Mode game.' />
-                <Game name='Hellblade' year='2024' imageUrl='/2025/2025_2.jpg' specialDescription='this is a good game.' />
-                <Game name='Hellblade' year='2024' imageUrl='/2025/2025_3.jpg' specialDescription='this is a good game.' />
-                <Game name='Hellblade' year='2024' imageUrl='/2025/2025_4.jpg' specialDescription='this is a good game.' />
-                <Game name='Hellblade' year='2024' imageUrl='/2025/2025_5.jpg' specialDescription='this is a good game.' />
-                <Game name='Hellblade' year='2024' imageUrl='/2025/2025_6.jpg' specialDescription='this is a good game.' />
-                <Game name='Stalker 2' year='2024' imageUrl='/2025/2025_7.jpg' specialDescription='this is a good game.' />
+                <Game name='Assassins Creed Mirage' year='2023' imageUrl='/2025/2025_1.jpg' specialDescription='' />
+                <Game name='Sifu' year='2021' imageUrl='/2025/2025_2.jpg' specialDescription='' />
+                <Game name='Death Stranding' year='2019' imageUrl='/2025/2025_3.jpg' specialDescription='' />
+                <Game name='It Takes Two' year='2021' imageUrl='/2025/2025_4.jpg' specialDescription='Game Of The Year - 2021' />
+                <Game name='The Guardians Of the Galaxy' year='2021' imageUrl='/2025/2025_5.jpg' specialDescription='' />
+                <Game name='Spider man 2' year='2023' imageUrl='/2025/2025_6.jpg' specialDescription='' />
+                <Game name='Stalker 2' year='2024' imageUrl='/2025/2025_7.jpg' specialDescription='' />
                 <Game name='The Last Of Us II' year='2020' imageUrl='/2025/2025_8.jpg' specialDescription='Ellie,Abby,joel,Dina,Jesse,Tommy,Liv.' />
                 <Game name='Clair Obscur - Expedition 33' year='2025' imageUrl='/2025/2025_9.jpg' specialDescription='Maelle,Gustave,Sciel,Lune,Verso,Alicia,   Monoco,Esquie,Renoir' />
+                <Game name='Call Of Duty MW II' year='2022' imageUrl='/2025/2025_10.jpg' specialDescription='' />
+                <Game name='Call Of Duty Vanguard' year='2021' imageUrl='/2025/2025_11.jpg' specialDescription='' />
+                <Game name='Need For Speed Unbound' year='2022' imageUrl='/2025/2025_12.jpg' specialDescription='' />
+                <Game name='Final Fantasy : Rebirth' year='2024' imageUrl='/2025/2025_13.jpg' specialDescription='' />
+                <Game name='Split Fiction' year='2025' imageUrl='/2025/2025_14.jpg' specialDescription='' />
+                <Game name='Kingdom Come Deliverance II' year='2025' imageUrl='/2025/2025_15.jpg' specialDescription='' />
             </div>
         </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,69 @@
+'use client'
 import Link from "next/link";
 
 export default function Home() {
+
+  const webUrl:String = "http://localhost:8080";
+
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  const handleSubmit = (event:any) => {
+    event.preventDefault();
+  
+  const data = {
+    name: String(event.target.name.value),
+    year: Number(event.target.year.value),
+    completedYear: Number(event.target.completedYear.value),
+    isCompleted: Boolean(event.target.isCompleted.value),
+    isHundredPercent: Boolean(event.target.isHundredPercent.value),
+    isFavourite: Boolean(event.target.isFavourite.value),
+    specialDescription: String(event.target.specialDescription.value),
+  }
+
+  }
+
   return (
-    <div >
-      <Link href="/Favourites">Click to see 2024 page</Link><br/>
-      <Link href="/FullyCompleted">Click to see 2024 page</Link><br/>
-      <Link href="/2025" >Click to see 2025 page</Link><br/>
-      <Link href="/2024">Click to see 2024 page</Link><br/>
-      
+    <div>
+
+      <Link href="/Favourites">Click to see Favourite Games</Link><br/>
+      <Link href="/FullyCompleted">Click to see 100% Completed Games</Link><br/>
+      <Link href="/2025" >Click to see Games Completed in 2025</Link><br/>
+      <Link href="/2024">Click to see Games Completed in 2024</Link><br/>
+
+      <div className="flex justify-center">
+        <form onSubmit={handleSubmit}>
+
+            <h1>Fill This To Add a New Game</h1><br/>
+
+            <label>Name : </label>
+            <input className="bg-amber-50 rounded-b-md rounded-t-sm" type="text" autoComplete="off" id="name" required minLength={1} maxLength={40}  /><br/><br/>
+
+            <label>Year : </label>
+            <input className="bg-amber-50 rounded-b-md rounded-t-sm" type="number" autoComplete="off" id="year" required defaultValue={currentYear} min={1975}  /><br/><br/>
+
+            <label>Completed Year/Played year : </label>
+            <input className="bg-amber-50 rounded-b-md rounded-t-sm" type="number" autoComplete="off" id="completedYear" defaultValue={currentYear} required min={1975} /><br/><br/>
+
+            <label>Is It Completed? : </label>
+            <input className="bg-amber-50 rounded-b-md rounded-t-sm" type="checkbox" id="isCompleted"/><br/><br/>
+
+            <label>Is It 100% Completed? : </label>
+            <input className="bg-amber-50 rounded-b-md rounded-t-sm" type="checkbox" id="isHundredPercent"/><br/><br/>
+
+            <label>Is It One of your Faourites? : </label>
+            <input className="bg-amber-50 rounded-b-md rounded-t-sm" type="checkbox" id="isFavourite"/><br/><br/>
+
+            <label>Special Description : </label> 
+            <input className="bg-amber-50 rounded-b-md rounded-t-sm" type="text" autoComplete="off" id="specialDescription" minLength={0} maxLength={40}  /><br/><br/>
+
+            <label>Add image Here :</label>
+            <div></div><br/>
+
+            <button className="bg-amber-950 p-2 rounded-md" type="submit">Submit</button>
+
+        </form>
+      </div>
     </div>
   );
 }
