5631033c69b75443562b76a54cc10295b25c23cb
Sajeewa30 <sajeewadilshan30@gmail.com>
2026-01-06 02:10:26 +0530

    Update Next config for Next 16

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     // Allow serving S3-hosted cover art.
-    domains: [
-      'localhost',
-      'game-tracker-sajeewa.s3.eu-north-1.amazonaws.com',
-    ],
     remotePatterns: [
       {
         protocol: 'https',
